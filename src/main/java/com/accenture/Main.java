2f7eb07a102bcb462d21332cfb6cee5e5edc47ea
Kuchcik <b.kuchcik@accenture.com>
2021-10-18 12:26:02 +0200

    initial version of project

---
+++ src/main/java/com/accenture/Main.java
@@ -0,0 +1,8 @@
+package com.accenture;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Code me please");
+    }
+}
---
+++ src/test/java/com/accenture/FizzBuzzTest.java
@@ -0,0 +1,11 @@
+package com.accenture;
+
+import org.junit.jupiter.api.Test;
+
+public class FizzBuzzTest {
+
+    @Test
+    public void monPremierTest() {
+        System.out.println("Je ne fais rien");
+    }
+}
